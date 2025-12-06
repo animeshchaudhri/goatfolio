88de8d9294c43432271d8a04f25e304af8e1cf06
animeshchaudri <animeshchaudhri3@gmail.com>
2025-12-06 21:42:01 +0530

    feat speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
 import SEOBoost from "@/components/SEO/SEOBoost";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -98,6 +99,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <SEOBoost />
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
